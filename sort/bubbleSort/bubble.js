dd10e596cf7acadd67ea159cd78c0dcc4b3bd723
charliepatronr <54086538+charliepatronr@users.noreply.github.com>
2021-01-30 23:34:11 -0600

    optimized buuble sort

--- sort/bubbleSort/bubble.js
+++ sort/bubbleSort/bubble.js
@@ -28,3 +28,23 @@ function bubble(arr){
 
 
 console.log(bubble([37,45,29,8]))
+
+//Opmtimized bubble sort
+// uses conditional to avoid unecessary loops and comparisons 
+function bubble(arr){
+
+    let noSwaps
+    for(let i= arr.length -1; i>=0 ; i--){
+        noSwaps = true
+        for(let j = 0; j <= i - 1; j++ ){
+            if(arr[j] > arr[j + 1]){
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+                noSwaps = false
+            }
+        }
+        if(noSwaps) break;
+    }
+    return arr
+}
